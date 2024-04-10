737cee88638b9ac8f9b330aef985cd7e8d204739
Pawel Bialczak <pawelnwz@gmail.com>
2024-04-10 18:29:16 +0200

    Added first feature file

---
+++ src/test/java/com/clickup/gui/steps/LoginStepDefs.java
@@ -0,0 +1,26 @@
+package com.clickup.gui.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefs {
+
+    @Given("I am on base page")
+    public void i_am_on_base_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I click login button")
+    public void i_click_login_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("login form is displayed")
+    public void login_form_is_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
---
+++ src/test/resources/features/login.feature
@@ -0,0 +1,8 @@
+# language: en
+
+Feature: Sample feature
+
+  Scenario: Navigate to login page
+    Given I am on base page
+    When I click login button
+    Then login form is displayed
