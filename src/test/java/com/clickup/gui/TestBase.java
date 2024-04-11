b3944bf709dfee56cbd0136f17e5d8be83d9e587
Pawel Bialczak <pawelnwz@gmail.com>
2024-04-11 23:10:38 +0200

    Fixed teardown not killing some chrome instances

--- src/test/java/com/clickup/gui/TestBase.java
+++ src/test/java/com/clickup/gui/TestBase.java
@@ -3,10 +3,12 @@ package com.clickup.gui;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.AfterAll;
 
 public class TestBase {
 
     private Logger logger = LogManager.getLogger(this.getClass().getName());
 
     protected Logger log() { return logger; }
+
 }
