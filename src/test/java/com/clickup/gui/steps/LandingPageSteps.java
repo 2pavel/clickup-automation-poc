632a400f5019f59bf54ec748835816cda08b1dc3
Pawel Bialczak <pawelnwz@gmail.com>
2024-04-11 20:50:54 +0200

    Added logging

--- src/test/java/com/clickup/gui/steps/LandingPageSteps.java
+++ src/test/java/com/clickup/gui/steps/LandingPageSteps.java
@@ -1,12 +1,14 @@
 package com.clickup.gui.steps;
 
+import com.clickup.gui.TestBase;
 import com.clickup.gui.pages.LandingPage;
 
-public class LandingPageSteps {
+public class LandingPageSteps extends TestBase {
 
     LandingPage landingPage;
 
     public void open() {
+        log().info("Opening landing page");
         landingPage.open();
     }
 }
--- src/test/java/com/clickup/gui/steps/LoginSteps.java
+++ src/test/java/com/clickup/gui/steps/LoginSteps.java
@@ -1,12 +1,14 @@
 package com.clickup.gui.steps;
 
+import com.clickup.gui.TestBase;
 import com.clickup.gui.pages.LandingPage;
 
-public class LoginSteps {
+public class LoginSteps extends TestBase {
 
     LandingPage landingPage;
 
     public void openLoginForm() {
+        log().info("Clicking login button");
         landingPage.clickLogin();
     }
 }
