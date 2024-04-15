44c149791628be6304ab6832a8c8aa9c2612dc6b
Pawel Bialczak <pawelnwz@gmail.com>
2024-04-15 15:31:41 +0200

    added common methods for GUI

--- src/test/java/com/clickup/gui/utils/CommonMethodsGUI.java
+++ src/test/java/com/clickup/gui/utils/CommonMethodsGUI.java
@@ -1,4 +1,23 @@
 package com.clickup.gui.utils;
 
+import net.serenitybdd.core.pages.WebElementFacade;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+import static net.serenitybdd.core.Serenity.getDriver;
+
 public class CommonMethodsGUI {
+
+    private static JavascriptExecutor javascriptExecutor;
+
+    public static void markElementWithColor(WebElementFacade element) {
+        javascriptExecutor = (JavascriptExecutor) getDriver();
+        javascriptExecutor.executeScript("arguments[0].style.backgroundColor = 'lightblue'", element);
+    }
+
+    public static void clickOnElement(WebElementFacade element) {
+        element.waitUntilClickable();
+        markElementWithColor(element);
+        element.click();
+    }
 }
