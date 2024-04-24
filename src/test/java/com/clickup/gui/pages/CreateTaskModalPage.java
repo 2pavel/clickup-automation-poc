1c6cf2e0a11d3ef4c0fe0454630a9207810c3938
Pawel Bialczak <pawelnwz@gmail.com>
2024-04-24 19:24:23 +0200

    Added page object for Task creation modal

---
+++ src/test/java/com/clickup/gui/pages/CreateTaskModalPage.java
@@ -0,0 +1,21 @@
+package com.clickup.gui.pages;
+
+import com.clickup.gui.utils.CommonMethodsGUI;
+import lombok.Getter;
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
+import org.openqa.selenium.support.FindBy;
+
+@Getter
+public class CreateTaskModalPage extends PageObject {
+
+    @FindBy(css = "[data-test=draft-view__quick-create-create]")
+    private WebElementFacade createTaskBtn;
+
+    @FindBy(css = "textarea[data-test=draft-view__title-task]")
+    private WebElementFacade taskNameField;
+
+    public void clickCreateTaskBtn() {
+        CommonMethodsGUI.clickOnElement(createTaskBtn);
+    }
+}
